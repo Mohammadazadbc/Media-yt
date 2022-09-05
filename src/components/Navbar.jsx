03b231127cbcbdb6a6515920e2e5eee3b4d563a6
mohammad azad <mohammadazad.bc@gmail.com>
2022-09-05 15:43:17 +0200

    init project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Stack}from "@mui/material"
+import {Link} from "react-router-dom"
+function Navbar() {
+  return (
+    <Stack>
+
+    </Stack>
+  )
+}
+
+export default Navbar
